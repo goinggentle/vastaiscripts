c92d76a76b748a1d8f33698716e9acba72f3eb55
goinggentle <goinggentle@proton.me>
2025-12-11 00:33:56 -0600

    update

--- infinitetalk_multi.sh
+++ infinitetalk_multi.sh
@@ -4,9 +4,10 @@
 # 120GB
 # vastai/pytorch:2.7.0-cuda-12.8.1-py310-22.04
 
-cd /workspace/
+touch ~/.no_auto_tmux
+cd /workspace
 git clone https://github.com/MeiGen-AI/InfiniteTalk.git
-cd /InfiniteTalk/
+cd InfiniteTalk
 
 pip install -U xformers==0.0.30
 pip install misaki[en]
@@ -20,11 +21,15 @@ pip install librosa
 pip install hf-transfer
 pip install transformers==4.52.4
 
-hf download Wan-AI/Wan2.1-I2V-14B-480P --local-dir ./weights/Wan2.1-I2V-14B-480P
-hf download TencentGameMate/chinese-wav2vec2-base --local-dir ./weights/chinese-wav2vec2-base
-hf download TencentGameMate/chinese-wav2vec2-base model.safetensors --revision refs/pr/1 --local-dir ./weights/chinese-wav2vec2-base
+hf download Wan-AI/Wan2.1-I2V-14B-480P \
+    --local-dir ./weights/Wan2.1-I2V-14B-480P
+hf download TencentGameMate/chinese-wav2vec2-base \
+    --local-dir ./weights/chinese-wav2vec2-base
+hf download TencentGameMate/chinese-wav2vec2-base model.safetensors \
+    --revision refs/pr/1 \
+    --local-dir ./weights/chinese-wav2vec2-base
 wget -O weights/Wan2.1_I2V_14B_FusionX_LoRA.safetensors \
-"https://huggingface.co/vrgamedevgirl84/Wan14BT2VFusioniX/resolve/main/FusionX_LoRa/Wan2.1_I2V_14B_FusionX_LoRA.safetensors"
+    "https://huggingface.co/vrgamedevgirl84/Wan14BT2VFusioniX/resolve/main/FusionX_LoRa/Wan2.1_I2V_14B_FusionX_LoRA.safetensors"
 mkdir -p /weights/InfiniteTalk/multi
 wget -O weights/InfiniteTalk/multi/infinitetalk.safetensors \
-"https://huggingface.co/MeiGen-AI/InfiniteTalk/resolve/main/multi/infinitetalk.safetensors"
+    "https://huggingface.co/MeiGen-AI/InfiniteTalk/resolve/main/multi/infinitetalk.safetensors"
